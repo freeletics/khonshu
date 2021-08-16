5cd81d67edec040511582ab854427e9f658e45e5
Gabriel Ittner <gabriel.ittner@gmail.com>
2021-08-16 08:39:07 +0200

    move NavEntry annotations and interface into one file

--- whetstone/runtime/src/main/java/com/freeletics/mad/whetstone/NavEntryComponent.kt
+++ whetstone/runtime/src/main/java/com/freeletics/mad/whetstone/NavEntryComponent.kt
@@ -1,7 +1,9 @@
 package com.freeletics.mad.whetstone
 
+import android.content.Context
+import androidx.navigation.NavBackStackEntry
+import javax.inject.Qualifier
 import kotlin.annotation.AnnotationRetention.RUNTIME
-import kotlin.annotation.AnnotationTarget.CLASS
 import kotlin.annotation.AnnotationTarget.FUNCTION
 import kotlin.reflect.KClass
 
@@ -39,7 +41,7 @@ import kotlin.reflect.KClass
  * It's recommended to put this annotation on the method that provides the matching [NavEntryId]
  * value.
  */
-@Target(CLASS, FUNCTION)
+@Target(FUNCTION)
 @Retention(RUNTIME)
 annotation class NavEntryComponent(
     val scope: KClass<*>,
@@ -48,3 +50,29 @@ annotation class NavEntryComponent(
     val coroutinesEnabled: Boolean = false,
     val rxJavaEnabled: Boolean = false,
 )
+
+/**
+ * A qualifier that should be used to provide a resource id that represents an androidx.navigation
+ * destination. This navigation id will be the one that a [NavEntryComponent], that uses the same
+ * scope as the given [value], is tied to.
+ *
+ * It is recommended to put [NavEntryComponent] onto the same method that uses this qualifier
+ * annotation.
+ */
+@Qualifier
+annotation class NavEntryId(val value: KClass<*>)
+
+/**
+ * A generated implementation of this can be used to retrieve a generated [NavEntryComponent].
+ *
+ * The implementation will be bound into a `Map<String, NavEntryComponentGetter` were the key
+ * is the same scope that is used in [NavEntryId] and [NavEntryComponent].
+ */
+interface NavEntryComponentGetter {
+    /**
+     * The id that is passed as parameter to [findEntry] is the id that was provided with
+     * [NavEntryId]. The given [findEntry] should look up a back strack entry for that id
+     * in the current `NavController`.
+     */
+    fun retrieve(findEntry: (Int) -> NavBackStackEntry, context: Context): Any
+}
