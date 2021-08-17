a8414e4f34af5f3e5d1d5f5fd2384158e82ecf76
Timo Loewe <timo@freeletics.com>
2021-08-17 15:07:18 +0200

    Small fixes for NavEntryComponent documentation (#4)

--- whetstone/runtime/src/main/java/com/freeletics/mad/whetstone/NavEntryComponent.kt
+++ whetstone/runtime/src/main/java/com/freeletics/mad/whetstone/NavEntryComponent.kt
@@ -12,9 +12,9 @@ import kotlin.reflect.KClass
  * survives configuration changes and is kept as long as a specific `NavBackStackEntry`
  * is on the back stack.
  *
- * The generated component uses [ScopeTo] as it's scope where the [ScopeTo.marker]
+ * The generated component uses [ScopeTo] as its scope where the [ScopeTo.marker]
  * parameter is the specified [scope] class. This scope can be used to scope classes
- * in the component an tie them to to component's life time. The annotated class is also used as
+ * in the component and tie them to to component's life time. The annotated class is also used as
  * [com.squareup.anvil.annotations.MergeComponent.scope], so it can be used to contribute modules
  * and bindings to the generated component.
  *
@@ -26,10 +26,10 @@ import kotlin.reflect.KClass
  * By default the following classes are available for injection in the component:
  * - a [androidx.lifecycle.SavedStateHandle]
  * - a [android.os.Bundle] obtained from [androidx.navigation.NavController.currentBackStackEntry]
- * - if [coroutinesEnabled] is `true`, a [io.reactivex.disposables.CompositeDisposable] that will be cleared automatically
- * - if [rxJavaEnabled] is `true`, a [kotlinx.coroutines.CoroutineScope] that will be cancelled automatically
+ * - if [coroutinesEnabled] is `true`, a [kotlinx.coroutines.CoroutineScope] that will be cancelled automatically
+ * - if [rxJavaEnabled] is `true`, a [io.reactivex.disposables.CompositeDisposable] that will be cleared automatically
  *
- * The component that uses [parentScope] as it's scope will be used as parent component. That
+ * The component that uses [parentScope] as its scope will be used as parent component. That
  * component will be looked up by calling `getSystemService` on the current `Activity` and
  * the `Application`. The parameter passed to `getSystemService` is the fully qualified name of
  * the [parentScope] class. It is recommended to use the same marker class that is used as Anvil
