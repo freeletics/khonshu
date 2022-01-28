a84c88344ca6c50d10208d1de4f8d33ed2699e5c
Gabriel Ittner <gabriel@freeletics.com>
2022-01-28 16:24:50 +0100

    Update navigator/runtime-compose/src/main/java/com/freeletics/mad/navigator/compose/NavEventNavigationHandler.kt
    Co-authored-by: Kirill Boyarshinov <kirill@freeletics.com>

--- navigator/runtime-compose/src/main/java/com/freeletics/mad/navigator/compose/NavEventNavigationHandler.kt
+++ navigator/runtime-compose/src/main/java/com/freeletics/mad/navigator/compose/NavEventNavigationHandler.kt
@@ -128,7 +128,7 @@ public open class NavEventNavigationHandler : NavigationHandler<NavEventNavigato
             }
             is NavEvent.NavigateToTabEvent -> {
                 val options = NavOptions.Builder()
-                    // save the state of the current tab before leaving it
+                    // save the state of the current root before leaving it
                     .setPopUpTo(controller.graph.startDestinationId, inclusive = false, saveState = true)
                     // restoring the state of the target tab
                     .setRestoreState(event.restoreTabState)
