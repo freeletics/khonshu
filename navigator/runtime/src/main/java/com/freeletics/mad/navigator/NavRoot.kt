591ce35cf6026d63ae60f6ad5c13144c4fdf67a4
Gabriel Ittner <gabriel.ittner@gmail.com>
2022-02-07 13:12:50 +0100

    replace getArguments() by making NavRoute Parcelable and convert it internally

--- navigator/runtime/src/main/java/com/freeletics/mad/navigator/NavRoot.kt
+++ navigator/runtime/src/main/java/com/freeletics/mad/navigator/NavRoot.kt
@@ -1,22 +1,15 @@
 package com.freeletics.mad.navigator
 
-import android.os.Bundle
+import android.os.Parcelable
 
 /**
  * This is similar to a [NavRoute] but represents the route to the start destination used in
  * a backstack. When you navigate to a [NavRoot] the current backstack is saved and removed
  * so that the [NavRoot] is right on top of the start destination.
- *
- * [getArguments] can optionally be overridden to provide extra information to the destination.
  */
-public interface NavRoot {
+public interface NavRoot : Parcelable {
     /**
      * The destination this route leads to.
      */
     public val destinationId: Int
-
-    /**
-     * Arguments provided to the destination. For example an id.
-     */
-    public fun getArguments(): Bundle = Bundle.EMPTY
 }
