074ae26cf2c6dad53ab26f7c8ee894c26be26b14
Gabriel Ittner <gabriel.ittner@gmail.com>
2022-02-08 15:17:04 +0100

    remove Parcelable from NavRoute/NavRoot interfaces (#38)

--- navigator/runtime/src/main/java/com/freeletics/mad/navigator/NavRoot.kt
+++ navigator/runtime/src/main/java/com/freeletics/mad/navigator/NavRoot.kt
@@ -6,8 +6,11 @@ import android.os.Parcelable
  * This is similar to a [NavRoute] but represents the route to the start destination used in
  * a backstack. When you navigate to a [NavRoot] the current backstack is saved and removed
  * so that the [NavRoot] is right on top of the start destination.
+ *
+ * When the implementing class is [Parcelable], the instance of route will be put into the
+ * navigation arguments and is then available to the target screens.
  */
-public interface NavRoot : Parcelable {
+public interface NavRoot {
     /**
      * The destination this route leads to.
      */
--- navigator/runtime/src/main/java/com/freeletics/mad/navigator/NavRoute.kt
+++ navigator/runtime/src/main/java/com/freeletics/mad/navigator/NavRoute.kt
@@ -4,8 +4,11 @@ import android.os.Parcelable
 
 /**
  * Represents the route to a destination.
+ *
+ * When the implementing class is [Parcelable], the instance of route will be put into the
+ * navigation arguments and is then available to the target screens.
  */
-public interface NavRoute : Parcelable {
+public interface NavRoute {
     /**
      * The destination this route leads to.
      */
