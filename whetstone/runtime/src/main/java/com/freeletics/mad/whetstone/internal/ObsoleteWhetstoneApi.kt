425f55f0eada0b8f62d4b5fdd0c07028f27b21c4
Gabriel Ittner <gabriel.ittner@gmail.com>
2022-02-09 14:56:19 +0100

    add DestinationType to @NavDestination

---
+++ whetstone/runtime/src/main/java/com/freeletics/mad/whetstone/internal/ObsoleteWhetstoneApi.kt
@@ -0,0 +1,8 @@
+package com.freeletics.mad.whetstone.internal
+
+/**
+ * Code marked with [ObsoleteWhetstoneApi] has no guarantees about API stability and will be removed
+ * in a future release.
+ */
+@RequiresOptIn
+public annotation class ObsoleteWhetstoneApi
