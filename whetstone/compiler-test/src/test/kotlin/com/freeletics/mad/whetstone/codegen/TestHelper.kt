0ee32ba20e62ec02d0c878c6ce6f44afcdefe03b
Gabriel Ittner <gabriel.ittner@gmail.com>
2023-01-13 18:08:48 +0100

    run Anvil in all codegen tests

--- whetstone/compiler-test/src/test/kotlin/com/freeletics/mad/whetstone/codegen/TestHelper.kt
+++ whetstone/compiler-test/src/test/kotlin/com/freeletics/mad/whetstone/codegen/TestHelper.kt
@@ -5,39 +5,117 @@ import com.freeletics.mad.whetstone.ComposeFragmentData
 import com.freeletics.mad.whetstone.ComposeScreenData
 import com.freeletics.mad.whetstone.RendererFragmentData
 import com.google.common.truth.Truth.assertThat
+import com.squareup.anvil.compiler.AnvilCommandLineProcessor
+import com.squareup.anvil.compiler.AnvilComponentRegistrar
 import com.tschuchort.compiletesting.KotlinCompilation
 import com.tschuchort.compiletesting.KotlinCompilation.ExitCode
+import com.tschuchort.compiletesting.PluginOption
 import com.tschuchort.compiletesting.SourceFile
+import java.io.File
+import java.nio.file.Files
 
-public fun test(data: ComposeScreenData, expected: String) {
+public fun test(data: ComposeScreenData, fileName: String, source: String, expected: String) {
     val actual = FileGenerator().generate(data).toString()
     assertThat(actual).isEqualTo(expected)
-    compile(actual)
+    compile(fileName, source, actual)
+    compileWithAnvil(fileName, source, actual)
 }
 
-public fun test(data: ComposeFragmentData, expected: String) {
+public fun test(data: ComposeFragmentData, fileName: String, source: String, expected: String) {
     val actual = FileGenerator().generate(data).toString()
     assertThat(actual).isEqualTo(expected)
-    compile(actual)
+    compile(fileName, source, actual)
+    compileWithAnvil(fileName, source, actual)
 }
 
-public fun test(data: RendererFragmentData, expected: String) {
+public fun test(data: RendererFragmentData, fileName: String, source: String, expected: String) {
     val actual = FileGenerator().generate(data).toString()
     assertThat(actual).isEqualTo(expected)
-    compile(actual)
+    compile(fileName, source, actual)
+    compileWithAnvil(fileName, source, actual)
 }
 
-private fun compile(source: String) {
-    val result = KotlinCompilation().apply {
-        sources = listOf(SourceFile.kotlin("WhetstoneTest.kt", source))
-        compilerPlugins = listOf(ComposeComponentRegistrar())
-        kotlincArguments = listOf(
-            "-P",
-            "plugin:androidx.compose.compiler.plugins.kotlin:suppressKotlinVersionCompatibilityCheck=1.7.22"
+private fun compile(fileName: String, source: String, output: String) {
+    val compilation = KotlinCompilation().apply {
+        configure()
+
+        sources = listOf(
+            sourceFile(fileName, source),
+            sourceFile(fileName.testFileName(), output),
         )
-        jvmTarget = "11"
-        inheritClassPath = true
-        messageOutputStream = System.out // see diagnostics in real time
-    }.compile()
-    assertThat(result.exitCode).isEqualTo(ExitCode.OK)
+    }
+
+    assertThat(compilation.compile().exitCode).isEqualTo(ExitCode.OK)
+}
+
+private fun compileWithAnvil(fileName: String, source: String, output: String) {
+    val compilation = KotlinCompilation().apply {
+        configure()
+        configureAnvil()
+
+        sources = listOf(
+            sourceFile(fileName, source),
+        )
+    }
+
+    assertThat(compilation.compile().exitCode).isEqualTo(ExitCode.OK)
+    assertThat(compilation.generatedFile(fileName)).isEqualTo(output)
+}
+
+private fun KotlinCompilation.sourceFile(name: String, content: String): SourceFile {
+    val path = "${workingDir.absolutePath}/sources/src/main/kotlin/$name"
+    Files.createDirectories(File(path).parentFile!!.toPath())
+    return SourceFile.kotlin(path, content)
+}
+
+private fun KotlinCompilation.generatedFile(name: String): String {
+    val path = "${workingDir.absolutePath}/build/anvil/${name.testFileName()}"
+    return File(path).readText()
+}
+
+private fun String.testFileName(): String {
+    val path = substringBeforeLast("/")
+    val name = substringAfterLast("/")
+    return "$path/Whetstone$name"
+}
+
+private fun KotlinCompilation.configure() {
+    compilerPlugins = compilerPlugins + listOf(ComposeComponentRegistrar())
+    kotlincArguments = kotlincArguments + listOf(
+        "-P",
+        "plugin:androidx.compose.compiler.plugins.kotlin:suppressKotlinVersionCompatibilityCheck=1.7.22"
+    )
+    jvmTarget = "11"
+    inheritClassPath = true
+    messageOutputStream = System.out // see diagnostics in real time
+}
+
+private fun KotlinCompilation.configureAnvil() {
+    compilerPlugins = compilerPlugins + listOf(AnvilComponentRegistrar())
+
+    val anvilCommandLineProcessor = AnvilCommandLineProcessor()
+    commandLineProcessors = listOf(anvilCommandLineProcessor)
+
+    pluginOptions = listOf(
+        PluginOption(
+            pluginId = anvilCommandLineProcessor.pluginId,
+            optionName = "src-gen-dir",
+            optionValue = File(workingDir, "build/anvil").absolutePath
+        ),
+        PluginOption(
+            pluginId = anvilCommandLineProcessor.pluginId,
+            optionName = "generate-dagger-factories",
+            optionValue = "false"
+        ),
+        PluginOption(
+            pluginId = anvilCommandLineProcessor.pluginId,
+            optionName = "generate-dagger-factories-only",
+            optionValue = "false"
+        ),
+        PluginOption(
+            pluginId = anvilCommandLineProcessor.pluginId,
+            optionName = "disable-component-merging",
+            optionValue = "false"
+        )
+    )
 }
--- whetstone/compiler-test/src/test/kotlin/com/test/TestFixtures.kt
+++ whetstone/compiler-test/src/test/kotlin/com/test/TestFixtures.kt
@@ -2,12 +2,9 @@ package com.test
 
 import android.os.Parcel
 import android.view.View
-import androidx.compose.runtime.Composable
 import androidx.viewbinding.ViewBinding
 import com.freeletics.mad.navigator.NavRoute
 import com.freeletics.mad.statemachine.StateMachine
-import com.gabrielittner.renderer.ViewRenderer
-import com.test.other.TestClass2
 import kotlinx.coroutines.flow.Flow
 
 public class TestScreen
@@ -18,30 +15,8 @@ public class TestRoute : NavRoute {
     override fun writeToParcel(p0: Parcel, p1: Int) {}
 }
 
-@Suppress("UNUSED_PARAMETER")
-@Composable
-public fun Test(
-    state: TestState,
-    sendAction: (TestAction) -> Unit
-) {}
-
-@Suppress("UNUSED_PARAMETER")
-@Composable
-public fun Test2(
-    testClass: TestClass,
-    test: TestClass2,
-    state: TestState,
-    sendAction: (TestAction) -> Unit
-) {}
-
 public class TestBinding : ViewBinding {
     override fun getRoot(): View = throw UnsupportedOperationException("Not implemented")
-}
-
-public class TestRenderer(view: View) : ViewRenderer<TestState, TestAction>(view) {
-    override fun renderToView(state: TestState) {}
-
-    public abstract class Factory : ViewRenderer.Factory<TestBinding, TestRenderer>({ _, _, _ -> TestBinding() })
 }
 
 public class TestStateMachine : StateMachine<TestState, TestAction> {
