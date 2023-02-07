4be07c2e9dc73a00fc8da2c937deac502c398ee9
Kirill Boyarshinov <kirill@freeletics.com>
2023-02-07 17:00:52 +0100

    Migrate settings.gradle to .kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,13 +16,13 @@ dependencyResolutionManagement {
     // repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
 }
 
-include ":navigator:runtime", ":navigator:testing"
-include ":navigator:androidx-nav", ":navigator:runtime-compose", ":navigator:runtime-fragment"
-include ":state-machine", ":state-machine:testing"
-include ":text-resource"
-include ":whetstone:compiler", ":whetstone:compiler-test"
-include ":whetstone:runtime", ":whetstone:runtime-compose", ":whetstone:runtime-fragment"
-include ":whetstone:navigation", ":whetstone:navigation-compose", ":whetstone:navigation-fragment"
+include(":navigator:runtime", ":navigator:testing")
+include(":navigator:androidx-nav", ":navigator:runtime-compose", ":navigator:runtime-fragment")
+include(":state-machine", ":state-machine:testing")
+include(":text-resource")
+include(":whetstone:compiler", ":whetstone:compiler-test")
+include(":whetstone:runtime", ":whetstone:runtime-compose", ":whetstone:runtime-fragment")
+include(":whetstone:navigation", ":whetstone:navigation-compose", ":whetstone:navigation-fragment")
 
 project(":navigator:runtime").setName("navigator-runtime")
 project(":navigator:runtime-compose").setName("navigator-runtime-compose")
