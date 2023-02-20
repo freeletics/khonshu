08fd25385bc1e4bc8354451136e811c49cc77fed
Gabriel Ittner <gabriel.ittner@gmail.com>
2023-02-20 10:37:22 +0100

    clean up whetstone parsing code a bit (#311)

--- whetstone/compiler/src/main/kotlin/com/freeletics/mad/whetstone/parser/ComposeParser.kt
+++ whetstone/compiler/src/main/kotlin/com/freeletics/mad/whetstone/parser/ComposeParser.kt
@@ -2,15 +2,9 @@ package com.freeletics.mad.whetstone.parser
 
 import com.freeletics.mad.whetstone.ComposeScreenData
 import com.freeletics.mad.whetstone.Navigation
-import com.freeletics.mad.whetstone.codegen.FileGenerator
 import com.freeletics.mad.whetstone.codegen.util.composeFqName
-import com.freeletics.mad.whetstone.codegen.util.composeNavDestinationFqName
-import com.freeletics.mad.whetstone.codegen.util.composeRootNavDestinationFqName
 import com.freeletics.mad.whetstone.codegen.util.whetstoneComposeScreenDestinationFqName
 import com.squareup.anvil.annotations.ExperimentalAnvilApi
-import com.squareup.anvil.compiler.api.GeneratedFile
-import com.squareup.anvil.compiler.api.createGeneratedFile
-import com.squareup.anvil.compiler.internal.reference.AnnotatedReference
 import com.squareup.anvil.compiler.internal.reference.TopLevelFunctionReference
 
 @OptIn(ExperimentalAnvilApi::class)
@@ -49,31 +43,4 @@ internal fun TopLevelFunctionReference.toComposeScreenDestinationData(): Compose
         navEntryData = navEntryData(navigation),
         composableParameter = composeParameters
     )
-}
-
-@OptIn(ExperimentalAnvilApi::class)
-private fun AnnotatedReference.composeNavigation(): Navigation.Compose? {
-    val navigation = findAnnotation(composeNavDestinationFqName)
-    if (navigation != null) {
-        val route = navigation.requireClassArgument("route", 0)
-        val destinationScope = navigation.requireClassArgument("destinationScope", 2)
-        return Navigation.Compose(
-            route = route,
-            destinationType = navigation.requireEnumArgument("type", 1),
-            destinationScope = destinationScope,
-        )
-    }
-
-    val rootNavigation = findAnnotation(composeRootNavDestinationFqName)
-    if (rootNavigation != null) {
-        val route = rootNavigation.requireClassArgument("root", 0)
-        val destinationScope = rootNavigation.requireClassArgument("destinationScope", 1)
-        return Navigation.Compose(
-            route = route,
-            destinationType = "SCREEN",
-            destinationScope = destinationScope,
-        )
-    }
-
-    return null
 }
