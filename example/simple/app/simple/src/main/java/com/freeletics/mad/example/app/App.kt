b0d49322249cdbe5fe157856bc2de91a54ea4927
Frank Hermann <hermann.frank@gmail.com>
2023-03-23 09:57:14 +0100

    Review: Use lazy value for AppComponent

--- example/simple/app/simple/src/main/java/com/freeletics/mad/example/app/App.kt
+++ example/simple/app/simple/src/main/java/com/freeletics/mad/example/app/App.kt
@@ -4,21 +4,14 @@ import android.app.Application
 import com.freeletics.mad.whetstone.AppScope
 
 class App : Application() {
-    private var component: AppComponent? = null
-
     override fun getSystemService(name: String): Any? {
         if (name == AppScope::class.qualifiedName) {
-            return component()
+            return component
         }
         return super.getSystemService(name)
     }
 
-    private fun component(): AppComponent {
-        var component = this.component
-        if (component == null) {
-            component = AppComponent.factory().create(this)
-            this.component = component
-        }
-        return component
+    private val component by lazy {
+        AppComponent.factory().create(this)
     }
 }
