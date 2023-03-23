5ed3950aa36b6fc0fa6a186f342039b514c128e9
Frank Hermann <hermann.frank@gmail.com>
2023-03-23 10:12:02 +0100

    Review: Remove retained modules

--- sample/simple/feature/a/implementation/src/main/java/com/freeletics/mad/sample/feature/a/FeatureADI.kt
+++
@@ -1,9 +0,0 @@
-package com.freeletics.mad.sample.feature.a
-
-import com.freeletics.mad.sample.feature.a.nav.FeatureARoute
-import com.squareup.anvil.annotations.ContributesTo
-import dagger.Module
-
-@Module
-@ContributesTo(FeatureARoute::class)
-object FeatureARetainedModule
--- sample/simple/feature/main/src/main/java/com/freeletics/mad/sample/feature/main/MainDI.kt
+++
@@ -1,8 +0,0 @@
-package com.freeletics.mad.sample.feature.main
-
-import com.squareup.anvil.annotations.ContributesTo
-import dagger.Module
-
-@Module
-@ContributesTo(MainActivity::class)
-object MainRetainedModule
