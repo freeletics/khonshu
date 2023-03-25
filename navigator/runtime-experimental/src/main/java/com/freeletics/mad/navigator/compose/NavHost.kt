86db74be8e3f1bdead0d8aa15cd734ab02297236
Gabriel Ittner <gabriel.ittner@gmail.com>
2023-03-25 22:12:11 +0100

    NavHost: support rendering dialog and bottom sheet destinations

--- navigator/runtime-experimental/src/main/java/com/freeletics/mad/navigator/compose/NavHost.kt
+++ navigator/runtime-experimental/src/main/java/com/freeletics/mad/navigator/compose/NavHost.kt
@@ -2,9 +2,14 @@ package com.freeletics.mad.navigator.compose
 
 import androidx.activity.OnBackPressedCallback
 import androidx.activity.compose.LocalOnBackPressedDispatcherOwner
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.ModalBottomSheetDefaults
+import androidx.compose.material.ModalBottomSheetLayout
+import androidx.compose.material.ModalBottomSheetState
+import androidx.compose.material.ModalBottomSheetValue
 import androidx.compose.material.contentColorFor
+import androidx.compose.material.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.DisposableEffect
@@ -16,6 +21,7 @@ import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.window.Dialog
 import com.freeletics.mad.navigator.BaseRoute
 import com.freeletics.mad.navigator.DeepLinkHandler
 import com.freeletics.mad.navigator.NavRoot
@@ -27,15 +33,21 @@ import com.freeletics.mad.navigator.internal.NavigationExecutor
 import kotlinx.coroutines.flow.distinctUntilChanged
 
 /**
- * Create a new `NavHost containing all given [destinations]. [startRoute] will be used as the
+ * Create a new `NavHost` containing all given [destinations]. [startRoute] will be used as the
  * start destination of the graph. Use [com.freeletics.mad.navigator.NavEventNavigator] and
- * [NavigationSetup] to chage what is shown in [NavHost]
+ * [NavigationSetup] to change what is shown in [NavHost].
+ *
+ * To support deep links a set of [DeepLinkHandlers][DeepLinkHandler] can be passed in optionally.
+ * These will be used to build the correct back stack when the current `Activity` was launched with
+ * an `ACTION_VIEW` `Intent` that contains an url in it's data. [deepLinkPrefixes] can be used to
+ * provide a default set of url patterns that should be matched by any [DeepLinkHandler] that
+ * doesn't provide its own [DeepLinkHandler.prefixes].
  *
  * The [destinationChangedCallback] can be used to be notified when the current destination
  * changes. Note that this will not be invoked when navigating to a [ActivityDestination].
  */
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
-@Suppress("unused_parameter") //TODO
 public fun NavHost(
     startRoute: NavRoot,
     destinations: Set<NavDestination>,
@@ -65,17 +77,76 @@ public fun NavHost(
     CompositionLocalProvider(LocalNavigationExecutor provides executor) {
         val entries = executor.visibleEntries.value
 
-        Show(entries)
+        val bottomSheetEntry = entries.lastOrNull { it.destination is BottomSheetDestination<*> }
+        val modalBottomSheetState = rememberBottomSheetState(bottomSheetEntry, executor)
+
+        ModalBottomSheetLayout(
+            sheetContent = {
+                if (bottomSheetEntry != null) {
+                    Show(bottomSheetEntry)
+                }
+            },
+            sheetState = modalBottomSheetState,
+            sheetShape = bottomSheetShape,
+            sheetElevation = bottomSheetElevation,
+            sheetBackgroundColor = bottomSheetBackgroundColor,
+            sheetContentColor = bottomSheetContentColor,
+            scrimColor = bottomSheetScrimColor,
+        ) {
+            Show(entries, executor)
+        }
     }
+}
+
+@Composable
+@ExperimentalMaterialApi
+private fun rememberBottomSheetState(
+    bottomSheetEntry: StackEntry<*>?,
+    executor: MultiStackNavigationExecutor
+): ModalBottomSheetState {
+    val modalBottomSheetState = rememberModalBottomSheetState(
+        ModalBottomSheetValue.Hidden,
+        skipHalfExpanded = true,
+    )
+
+    LaunchedEffect(modalBottomSheetState, bottomSheetEntry) {
+        if (bottomSheetEntry != null) {
+            modalBottomSheetState.show()
+
+            snapshotFlow { modalBottomSheetState.isVisible }
+                .distinctUntilChanged()
+                .collect {
+                    if (!it) {
+                        executor.navigateBack()
+                    }
+                }
+        } else {
+            modalBottomSheetState.hide()
+        }
+    }
+
+    return modalBottomSheetState
 }
 
 @Composable
 private fun Show(
     entries: List<StackEntry<*>>,
+    executor: MultiStackNavigationExecutor,
 ) {
-    // TODO show all entries and differentiate between destination types
-    val entry = entries.last()
-    Show(entry)
+    entries.forEach { entry ->
+        when(entry.destination) {
+            is ScreenDestination -> {
+                Show(entry)
+            }
+            is DialogDestination<*> -> {
+                Dialog(onDismissRequest = { executor.navigateBack() }) {
+                    Show(entry)
+                }
+            }
+            // handled already in the layout
+            is BottomSheetDestination<*> -> {}
+        }
+    }
 }
 
 @Composable
