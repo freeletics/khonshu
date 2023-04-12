6bc5cb015018dd9625dc9d0863916d2898349a88
Gabriel Ittner <gabriel.ittner@gmail.com>
2023-04-12 18:10:56 +0200

    implement Stack which represents a single back stack (#369)
    * implement Stack which represents a single back stack
    
    * use error
    
    * make removable more specific

---
+++ navigator/runtime-experimental/src/main/java/com/freeletics/mad/navigator/compose/internal/StackEntry.kt
@@ -0,0 +1,31 @@
+package com.freeletics.mad.navigator.compose.internal
+
+import androidx.compose.runtime.Immutable
+import com.freeletics.mad.navigator.BaseRoute
+import com.freeletics.mad.navigator.NavRoot
+import com.freeletics.mad.navigator.NavRoute
+import com.freeletics.mad.navigator.compose.ContentDestination
+import com.freeletics.mad.navigator.internal.destinationId
+import dev.drewhamilton.poko.Poko
+
+@Poko
+@Immutable
+internal class StackEntry<T : BaseRoute>(
+    val id: Id,
+    val route: T,
+    val destination: ContentDestination<T>,
+){
+    val destinationId
+        get() = route.destinationId
+
+    val removable
+        // cast is needed for the compiler to recognize that the when is exhaustive
+        @Suppress("USELESS_CAST")
+        get() = when(route as BaseRoute) {
+            is NavRoute -> true
+            is NavRoot -> false
+        }
+
+    @JvmInline
+    value class Id(val value: String)
+}
---
+++ navigator/runtime-experimental/src/test/kotlin/com/freeletics/mad/navigator/compose/test/Destinations.kt
@@ -0,0 +1,18 @@
+package com.freeletics.mad.navigator.compose.test
+
+import com.freeletics.mad.navigator.compose.BottomSheetDestination
+import com.freeletics.mad.navigator.compose.DialogDestination
+import com.freeletics.mad.navigator.compose.ScreenDestination
+import com.freeletics.mad.navigator.internal.DestinationId
+
+internal val simpleRootDestination = ScreenDestination(DestinationId(SimpleRoot::class)) {}
+internal val simpleRouteDestination = ScreenDestination(DestinationId(SimpleRoute::class)) {}
+internal val otherRouteDestination = DialogDestination(DestinationId(OtherRoute::class)) {}
+internal val thirdRouteDestination = BottomSheetDestination(DestinationId(ThirdRoute::class)) {}
+
+internal val destinations = listOf(
+    simpleRootDestination,
+    simpleRouteDestination,
+    otherRouteDestination,
+    thirdRouteDestination,
+)
