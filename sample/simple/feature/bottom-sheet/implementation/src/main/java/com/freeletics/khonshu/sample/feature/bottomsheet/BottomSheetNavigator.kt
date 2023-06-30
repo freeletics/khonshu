f77f97e770bd92834b893db8968a1bd0f2de4089
Gabriel Ittner <gabriel.ittner@gmail.com>
2023-06-30 14:35:12 +0200

    missing import

--- sample/simple/feature/bottom-sheet/implementation/src/main/java/com/freeletics/khonshu/sample/feature/bottomsheet/BottomSheetNavigator.kt
+++ sample/simple/feature/bottom-sheet/implementation/src/main/java/com/freeletics/khonshu/sample/feature/bottomsheet/BottomSheetNavigator.kt
@@ -2,6 +2,7 @@ package com.freeletics.khonshu.sample.feature.bottomsheet
 
 import com.freeletics.khonshu.codegen.ScopeTo
 import com.freeletics.khonshu.navigation.NavEventNavigator
+import com.freeletics.khonshu.sample.feature.bottomsheet.nav.BottomSheetRoute
 import com.squareup.anvil.annotations.ContributesBinding
 import javax.inject.Inject
 
