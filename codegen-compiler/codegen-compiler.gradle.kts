a6a14f2c586d839b9f1fdd11193ee90f5be81c7b
Gabriel Ittner <gabriel.ittner@gmail.com>
2023-07-19 10:10:55 +0200

    update tests

--- codegen-compiler/codegen-compiler.gradle.kts
+++ codegen-compiler/codegen-compiler.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.fgp.jvm)
     alias(libs.plugins.kotlin.kapt)
     alias(libs.plugins.fgp.publish)
+    id("java-test-fixtures")
 }
 
 freeletics {
@@ -28,6 +29,12 @@ dependencies {
     testImplementation(libs.coroutines.core)
     testImplementation(libs.flowredux)
     testImplementation(testFixtures(libs.anvil.compiler.utils))
+
+    testFixturesApi(testFixtures(libs.anvil.compiler.utils))
+    testFixturesApi(libs.kotlin.compile.testing)
+    testFixturesImplementation(libs.anvil.compiler)
+    testFixturesImplementation(libs.dagger.compiler)
+    testFixturesImplementation("com.google.auto.value:auto-value:1.10.2")
 }
 
 // exclude external dependency on state machine connect, we include the local module instead
---
+++ codegen-compiler/src/testFixtures/kotlin/com/freeletics/khonshu/codegen/AnvilCompilation.kt
@@ -0,0 +1,242 @@
+// copy from Anvil until they release a version that uses compile testing 0.3.0
+package com.freeletics.khonshu.codegen
+
+import com.google.auto.value.processor.AutoAnnotationProcessor
+import com.squareup.anvil.annotations.ExperimentalAnvilApi
+import com.squareup.anvil.compiler.AnvilCommandLineProcessor
+import com.squareup.anvil.compiler.AnvilComponentRegistrar
+import com.squareup.anvil.compiler.api.CodeGenerator
+import com.tschuchort.compiletesting.JvmCompilationResult
+import com.tschuchort.compiletesting.KotlinCompilation
+import com.tschuchort.compiletesting.PluginOption
+import com.tschuchort.compiletesting.SourceFile
+import com.tschuchort.compiletesting.addPreviousResultToClasspath
+import dagger.internal.codegen.ComponentProcessor
+import org.intellij.lang.annotations.Language
+import org.jetbrains.kotlin.config.JvmTarget
+import java.io.File
+import java.io.OutputStream
+import java.nio.file.Files
+
+/**
+ * A simple API over a [KotlinCompilation] with extra configuration support for Anvil.
+ */
+@ExperimentalAnvilApi
+public class AnvilCompilation internal constructor(
+    public val kotlinCompilation: KotlinCompilation
+) {
+
+    private var isCompiled = false
+    private var anvilConfigured = false
+
+    /** Configures this the Anvil behavior of this compilation. */
+    @Suppress("SuspiciousCollectionReassignment")
+    @ExperimentalAnvilApi
+    public fun configureAnvil(
+        enableDaggerAnnotationProcessor: Boolean = false,
+        generateDaggerFactories: Boolean = false,
+        generateDaggerFactoriesOnly: Boolean = false,
+        disableComponentMerging: Boolean = false,
+        enableExperimentalAnvilApis: Boolean = true,
+        codeGenerators: List<CodeGenerator> = emptyList(),
+        enableAnvil: Boolean = true,
+    ): AnvilCompilation = apply {
+        checkNotCompiled()
+        check(!anvilConfigured) { "Anvil should not be configured twice." }
+
+        anvilConfigured = true
+
+        if (!enableAnvil) return@apply
+
+        kotlinCompilation.apply {
+            // Deprecation tracked in https://github.com/square/anvil/issues/672
+            @Suppress("DEPRECATION")
+            componentRegistrars = listOf(
+                AnvilComponentRegistrar().also { it.addCodeGenerators(codeGenerators) }
+            )
+            if (enableDaggerAnnotationProcessor) {
+                annotationProcessors = listOf(ComponentProcessor(), AutoAnnotationProcessor())
+            }
+
+            val anvilCommandLineProcessor = AnvilCommandLineProcessor()
+            commandLineProcessors = listOf(anvilCommandLineProcessor)
+
+            pluginOptions = listOf(
+                PluginOption(
+                    pluginId = anvilCommandLineProcessor.pluginId,
+                    optionName = "src-gen-dir",
+                    optionValue = File(workingDir, "build/anvil").absolutePath
+                ),
+                PluginOption(
+                    pluginId = anvilCommandLineProcessor.pluginId,
+                    optionName = "generate-dagger-factories",
+                    optionValue = generateDaggerFactories.toString()
+                ),
+                PluginOption(
+                    pluginId = anvilCommandLineProcessor.pluginId,
+                    optionName = "generate-dagger-factories-only",
+                    optionValue = generateDaggerFactoriesOnly.toString()
+                ),
+                PluginOption(
+                    pluginId = anvilCommandLineProcessor.pluginId,
+                    optionName = "disable-component-merging",
+                    optionValue = disableComponentMerging.toString()
+                )
+            )
+
+            if (enableExperimentalAnvilApis) {
+                kotlincArguments += listOf(
+                    "-opt-in=kotlin.RequiresOptIn",
+                    "-opt-in=com.squareup.anvil.annotations.ExperimentalAnvilApi"
+                )
+            }
+        }
+    }
+
+    /** Adds the given sources to this compilation with their packages and names inferred. */
+    public fun addSources(@Language("kotlin") vararg sources: String): AnvilCompilation = apply {
+        checkNotCompiled()
+        kotlinCompilation.sources += sources.mapIndexed { index, content ->
+            val packageDir = content.lines()
+                .firstOrNull { it.trim().startsWith("package ") }
+                ?.substringAfter("package ")
+                ?.replace('.', '/')
+                ?.let { "$it/" }
+                ?: ""
+
+            val name = "${kotlinCompilation.workingDir.absolutePath}/sources/src/main/java/" +
+                "$packageDir/Source$index.kt"
+
+            Files.createDirectories(File(name).parentFile.toPath())
+
+            SourceFile.kotlin(name, contents = content, trimIndent = true)
+        }
+    }
+
+    public fun addPreviousCompilationResult(result: JvmCompilationResult): AnvilCompilation = apply {
+        checkNotCompiled()
+        kotlinCompilation.addPreviousResultToClasspath(result)
+    }
+
+    public fun jvmTarget(jvmTarget: JvmTarget) {
+        checkNotCompiled()
+        kotlinCompilation.jvmTarget = jvmTarget.description
+    }
+
+    /**
+     * Returns an Anvil-generated file with the given [packageName] and [fileName] from its expected
+     * path.
+     */
+    public fun generatedAnvilFile(
+        packageName: String,
+        fileName: String
+    ): File {
+        check(isCompiled) {
+            "No compilation run yet! Call compile() first."
+        }
+        return File(
+            kotlinCompilation.workingDir,
+            "build/anvil/${packageName.replace('.', File.separatorChar)}/$fileName.kt"
+        )
+            .apply {
+                check(exists()) {
+                    "Generated file not found!"
+                }
+            }
+    }
+
+    private fun checkNotCompiled() {
+        check(!isCompiled) {
+            "Already compiled! Create a new compilation if you want to compile again."
+        }
+    }
+
+    /**
+     * Compiles the underlying [KotlinCompilation]. Note that if [configureAnvil] has not been called
+     * prior to this, it will be configured with default behavior.
+     */
+    public fun compile(
+        @Language("kotlin") vararg sources: String,
+        block: JvmCompilationResult.() -> Unit = {}
+    ): JvmCompilationResult {
+        checkNotCompiled()
+        if (!anvilConfigured) {
+            // Configure with default behaviors
+            configureAnvil()
+        }
+        addSources(*sources)
+        isCompiled = true
+
+        return kotlinCompilation.compile().apply(block)
+    }
+
+    public companion object {
+        public operator fun invoke(): AnvilCompilation {
+            return AnvilCompilation(
+                KotlinCompilation().apply {
+                    // Sensible default behaviors
+                    inheritClassPath = true
+                    jvmTarget = JvmTarget.JVM_1_8.description
+                    verbose = false
+                }
+            )
+        }
+    }
+}
+
+/**
+ * Helpful for testing code generators in unit tests end to end.
+ *
+ * This covers common cases, but is built upon reusable logic in [AnvilCompilation] and
+ * [AnvilCompilation.configureAnvil]. Consider using those APIs if more advanced configuration
+ * is needed.
+ */
+@ExperimentalAnvilApi
+public fun compileAnvil(
+    @Language("kotlin") vararg sources: String,
+    enableDaggerAnnotationProcessor: Boolean = false,
+    generateDaggerFactories: Boolean = false,
+    generateDaggerFactoriesOnly: Boolean = false,
+    disableComponentMerging: Boolean = false,
+    allWarningsAsErrors: Boolean = true,
+    messageOutputStream: OutputStream = System.out,
+    workingDir: File? = null,
+    enableExperimentalAnvilApis: Boolean = true,
+    previousCompilationResult: JvmCompilationResult? = null,
+    codeGenerators: List<CodeGenerator> = emptyList(),
+    moduleName: String? = null,
+    jvmTarget: JvmTarget? = null,
+    block: JvmCompilationResult.() -> Unit = { },
+): JvmCompilationResult {
+    return AnvilCompilation()
+        .apply {
+            kotlinCompilation.apply {
+                this.allWarningsAsErrors = allWarningsAsErrors
+                this.messageOutputStream = messageOutputStream
+                if (workingDir != null) {
+                    this.workingDir = workingDir
+                }
+                if (moduleName != null) {
+                    this.moduleName = moduleName
+                }
+            }
+
+            if (jvmTarget != null) {
+                jvmTarget(jvmTarget)
+            }
+
+            if (previousCompilationResult != null) {
+                addPreviousCompilationResult(previousCompilationResult)
+            }
+        }
+        .configureAnvil(
+            enableDaggerAnnotationProcessor = enableDaggerAnnotationProcessor,
+            generateDaggerFactories = generateDaggerFactories,
+            generateDaggerFactoriesOnly = generateDaggerFactoriesOnly,
+            disableComponentMerging = disableComponentMerging,
+            enableExperimentalAnvilApis = enableExperimentalAnvilApis,
+            codeGenerators = codeGenerators,
+        )
+        .compile(*sources)
+        .also(block)
+}
