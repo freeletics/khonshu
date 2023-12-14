04fe417aa5fe9e427d83b63be92c9d6ce5a68fdc
Gabriel Ittner <gabriel.ittner@gmail.com>
2023-12-14 15:42:26 +0100

    merge navigation-androidx into navigation-compose (#640)

--- navigation-compose/src/main/kotlin/com/freeletics/khonshu/navigation/androidx/internal/StoreViewModel.kt
+++ navigation-compose/src/main/kotlin/com/freeletics/khonshu/navigation/androidx/internal/StoreViewModel.kt
@@ -1,6 +1,8 @@
-package com.freeletics.khonshu.navigation.internal
+package com.freeletics.khonshu.navigation.androidx.internal
 
 import androidx.lifecycle.ViewModel
+import com.freeletics.khonshu.navigation.internal.NavigationExecutor
+import com.freeletics.khonshu.navigation.internal.NavigationExecutorStore
 
 internal class StoreViewModel private constructor(
     store: NavigationExecutorStore,
