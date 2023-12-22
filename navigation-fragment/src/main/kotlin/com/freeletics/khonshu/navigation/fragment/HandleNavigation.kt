5e105fac7d025292222dbc1303ebe10f37d98591
hoc081098@gmail.com <hoc081098@gmail.com>
2023-12-22 14:06:43 +0700

    Merge branch 'main' of https://github.com/hoc081098/khonshu into replace_all
    # Conflicts:
    #	navigation-compose/src/main/kotlin/com/freeletics/khonshu/navigation/compose/NavHost.kt
    #	navigation/api/android/navigation.api
    #	navigation/src/androidMain/kotlin/com/freeletics/khonshu/navigation/internal/NavigationExecutor.kt

--- navigation-fragment/src/main/kotlin/com/freeletics/khonshu/navigation/fragment/HandleNavigation.kt
+++ navigation-fragment/src/main/kotlin/com/freeletics/khonshu/navigation/fragment/HandleNavigation.kt
@@ -4,36 +4,13 @@ import android.app.Activity
 import androidx.activity.result.ActivityResultCaller
 import androidx.activity.result.ActivityResultLauncher
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.SavedStateHandle
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.coroutineScope
 import com.freeletics.khonshu.navigation.ContractResultOwner
 import com.freeletics.khonshu.navigation.NavEventNavigator
-import com.freeletics.khonshu.navigation.NavRoot
 import com.freeletics.khonshu.navigation.internal.collectAndHandleNavEvents
 import com.freeletics.khonshu.navigation.internal.collectAndHandleNavigationResults
 import com.freeletics.khonshu.navigation.internal.deliverResult
 import kotlinx.coroutines.launch
-
-internal class HandleNavigationViewModel(
-    private val savedStateHandle: SavedStateHandle,
-) : ViewModel() {
-    // This state is used to save the start route, so that we can update the start destination of the graph.
-    // It is updated when NavEventNavigation#replaceAll is called or when the `startRoute` parameter changes.
-    internal val savedStartRouteState = savedStateHandle.getStateFlow<NavRoot?>(
-        key = "SAVED_START_ROUTE",
-        initialValue = null,
-    )
-
-    init {
-        println("$this init")
-    }
-
-    fun onSaveStartRoute(navRoot: NavRoot) {
-        println("$this onSaveStartRoute $navRoot")
-        savedStateHandle["SAVED_START_ROUTE"] = navRoot
-    }
-}
 
 /**
  * Handles the [NavEventNavigator] events while the Fragment's lifecycle is at least
