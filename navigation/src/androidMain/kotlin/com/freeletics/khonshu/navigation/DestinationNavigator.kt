fcae82b9f8f7072eabbaacadc2b43459a63331bb
Gabriel Ittner <gabriel.ittner@gmail.com>
2024-06-10 15:24:42 +0200

    add navigate {} to DestinationNavigator

--- navigation/src/androidMain/kotlin/com/freeletics/khonshu/navigation/DestinationNavigator.kt
+++ navigation/src/androidMain/kotlin/com/freeletics/khonshu/navigation/DestinationNavigator.kt
@@ -5,5 +5,13 @@ package com.freeletics.khonshu.navigation
  * be used as base class for navigators of individual screens.
  */
 public abstract class DestinationNavigator(
-    navigator: HostNavigator,
-) : Navigator by navigator, ResultNavigator by navigator, BackInterceptor by navigator, ActivityResultNavigator()
+    private val navigator: HostNavigator,
+) : Navigator by navigator, ResultNavigator by navigator, BackInterceptor by navigator, ActivityResultNavigator() {
+
+    /**
+     * See [HostNavigator.navigate].
+     */
+    public fun navigate(block: Navigator.() -> Unit) {
+        navigator.navigate(block)
+    }
+}
