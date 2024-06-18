2a265686186b4fdc635146c2373b71a97e3b1773
Gabriel Ittner <gabriel.ittner@gmail.com>
2024-06-18 14:39:08 +0200

    fix sample

--- sample/simple/feature/dialog/implementation/src/main/kotlin/com/freeletics/khonshu/sample/feature/dialog/DialogNavigator.kt
+++ sample/simple/feature/dialog/implementation/src/main/kotlin/com/freeletics/khonshu/sample/feature/dialog/DialogNavigator.kt
@@ -1,6 +1,6 @@
 package com.freeletics.khonshu.sample.feature.dialog
 
-import com.freeletics.khonshu.navigation.ActivityResultNavigator
+import com.freeletics.khonshu.navigation.ActivityNavigator
 import com.freeletics.khonshu.navigation.DestinationNavigator
 import com.freeletics.khonshu.navigation.HostNavigator
 import com.freeletics.khonshu.sample.feature.dialog.nav.DialogRoute
@@ -11,5 +11,5 @@ import javax.inject.Inject
 
 @ForScope(DialogRoute::class)
 @SingleIn(DialogRoute::class)
-@ContributesBinding(DialogRoute::class, ActivityResultNavigator::class)
+@ContributesBinding(DialogRoute::class, ActivityNavigator::class)
 class DialogNavigator @Inject constructor(hostNavigator: HostNavigator) : DestinationNavigator(hostNavigator)
