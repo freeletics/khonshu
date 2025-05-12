4944358797791c17cb72fd8cca9c07e7127cea14
Gabriel Ittner <gabriel.ittner@gmail.com>
2025-05-12 22:26:45 +0200

    Kotlin 2.2.0

--- sample/simple/settings.gradle.kts
+++ sample/simple/settings.gradle.kts
@@ -3,15 +3,17 @@ pluginManagement {
         google()
         gradlePluginPortal()
         mavenCentral()
+        maven { setUrl("https://oss.sonatype.org/content/repositories/snapshots") }
     }
 }
 
 plugins {
-    id("com.freeletics.gradle.settings").version("0.25.0")
+    id("com.freeletics.gradle.settings").version("kotlin-2.2.0-SNAPSHOT")
 }
 
 rootProject.name = "simple-sample"
 
 freeletics {
+    snapshots()
     includeKhonshu("../..")
 }
