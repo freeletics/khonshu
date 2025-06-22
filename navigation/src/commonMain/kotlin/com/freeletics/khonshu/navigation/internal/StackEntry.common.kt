0a3f1b1e029ab813e564a24a75f93f3ccb6437ba
Gabriel Ittner <gabriel.ittner@gmail.com>
2025-06-22 10:56:47 +0200

    move Navigator to common

--- navigation/src/commonMain/kotlin/com/freeletics/khonshu/navigation/internal/StackEntry.common.kt
+++ navigation/src/commonMain/kotlin/com/freeletics/khonshu/navigation/internal/StackEntry.common.kt
@@ -1,7 +1,12 @@
 package com.freeletics.khonshu.navigation.internal
 
 import com.freeletics.khonshu.navigation.BaseRoute
+import kotlinx.serialization.Serializable
 
 public expect class StackEntry<T : BaseRoute> {
     public val route: T
+
+    @Serializable
+    @InternalNavigationTestingApi
+    public value class Id(internal val value: String)
 }
--- navigation/src/jvmMain/kotlin/com/freeletics/khonshu/navigation/internal/StackEntry.jvm.kt
+++ navigation/src/jvmMain/kotlin/com/freeletics/khonshu/navigation/internal/StackEntry.jvm.kt
@@ -1,8 +1,14 @@
 package com.freeletics.khonshu.navigation.internal
 
 import com.freeletics.khonshu.navigation.BaseRoute
+import kotlinx.serialization.Serializable
 
 public actual class StackEntry<T : BaseRoute> {
     public actual val route: T
         get() = TODO()
+
+    @JvmInline
+    @Serializable
+    @InternalNavigationTestingApi
+    public actual value class Id(internal val value: String)
 }
