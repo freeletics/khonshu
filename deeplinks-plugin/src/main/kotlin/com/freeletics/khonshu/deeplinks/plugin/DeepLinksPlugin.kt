d4b6c24e3f37b28cc94430aa0dfd8c749905deb8
Gabriel Ittner <gabriel.ittner@gmail.com>
2025-11-26 11:54:51 +0100

    support Android KMP plugin in deeplinks plugin

--- deeplinks-plugin/src/main/kotlin/com/freeletics/khonshu/deeplinks/plugin/DeepLinksPlugin.kt
+++ deeplinks-plugin/src/main/kotlin/com/freeletics/khonshu/deeplinks/plugin/DeepLinksPlugin.kt
@@ -9,30 +9,45 @@ import org.gradle.api.Project
  * Plugin for deeplinks integration:
  * - appends intent filters to app's manifest for deeplinks from configuration file
  */
-public abstract class DeeplinksPlugin : Plugin<Project> {
+public abstract class DeepLinksPlugin : Plugin<Project> {
     override fun apply(project: Project) {
         val extension = project.extensions.create("deepLinks", DeepLinksExtension::class.java)
         extension.deepLinkDefinitionsFile.convention(project.layout.projectDirectory.file("deeplinks.toml"))
 
+        var foundPlugin = false
         project.plugins.withId("com.android.library") {
-            setupDeeplinksManifestConfigurator(project, extension)
+            foundPlugin = true
+            setupDeepLinksManifestConfigurator(project, extension)
         }
 
         project.plugins.withId("com.android.application") {
-            setupDeeplinksManifestConfigurator(project, extension)
+            foundPlugin = true
+            setupDeepLinksManifestConfigurator(project, extension)
+        }
+
+        project.plugins.withId("com.android.kotlin.multiplatform.library") {
+            foundPlugin = true
+            setupDeepLinksManifestConfigurator(project, extension)
+        }
+
+        project.afterEvaluate {
+            check(foundPlugin) {
+                "No Android Gradle plugin was found. Please apply the 'com.android.library', " +
+                    "'com.android.application' or 'com.android.kotlin.multiplatform.library' plugin."
+            }
         }
     }
 
-    private fun setupDeeplinksManifestConfigurator(project: Project, extension: DeepLinksExtension) {
+    private fun setupDeepLinksManifestConfigurator(project: Project, extension: DeepLinksExtension) {
         val androidComponents = project.extensions.getByType(AndroidComponentsExtension::class.java)
         androidComponents.onVariants { variant ->
             val name = "${variant.name}DeeplinksManifestConfigurator"
-            val manifestUpdater = project.tasks.register(name, DeeplinksManifestConfiguratorTask::class.java) {
+            val manifestUpdater = project.tasks.register(name, DeepLinksManifestConfiguratorTask::class.java) {
                 it.deepLinkDefinitionsFile.set(extension.deepLinkDefinitionsFile)
             }
             variant.artifacts.use(manifestUpdater).wiredWithFiles(
-                DeeplinksManifestConfiguratorTask::mergedManifest,
-                DeeplinksManifestConfiguratorTask::updatedManifest,
+                DeepLinksManifestConfiguratorTask::mergedManifest,
+                DeepLinksManifestConfiguratorTask::updatedManifest,
             ).toTransform(SingleArtifact.MERGED_MANIFEST)
         }
     }
